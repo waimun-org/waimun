9fe133440154941c0a08f4bdc6ff395c8d152f3b
James Blair <james@blair.nz>
2025-05-29 18:08:26 +1200

    Refactor LowImpactHero and remove bottom border

--- components/blocks/hero/low-impact.tsx
+++ components/blocks/hero/low-impact.tsx
@@ -4,24 +4,22 @@ import { Button } from "@/components/button";
 
 export function LowImpactHero({ block }: HeroProps) {
   return (
-    <section className="border-b">
-      <div className="container flex flex-col gap-8 py-8">
-        <h1 className="text-2xl font-bold text-balance md:text-4xl">
-          {block.title}
-        </h1>
+    <section className="container flex flex-col gap-8 py-8">
+      <h1 className="text-2xl font-bold text-balance md:text-4xl">
+        {block.title}
+      </h1>
 
-        <div className="prose prose-lg text-balance">
-          <PortableText value={block.text} />
-        </div>
-
-        {block.buttons && (
-          <div className="flex flex-wrap gap-4">
-            {block.buttons.map((button) => (
-              <Button key={button._key} button={button} />
-            ))}
-          </div>
-        )}
+      <div className="prose prose-lg text-balance">
+        <PortableText value={block.text} />
       </div>
+
+      {block.buttons && (
+        <div className="flex flex-wrap gap-4">
+          {block.buttons.map((button) => (
+            <Button key={button._key} button={button} />
+          ))}
+        </div>
+      )}
     </section>
   );
 }
