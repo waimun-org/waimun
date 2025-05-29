78db7293fcff0b548f4b9de13ba063d3a78889a6
James Blair <james@blair.nz>
2025-05-29 18:53:08 +1200

    Remove unused slug queries from the queries file to streamline data fetching

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -7,10 +7,6 @@ export const PAGE_QUERY = groq`*[_type == "page" && slug.current == $slug][0] {
   }
 }`;
 
-export const PAGES_SLUGS_QUERY = groq`*[_type == "page" && defined(slug.current)]{
-  "slug": slug.current
-}`;
-
 export const NAVIGATION_QUERY = groq`*[_type == "navigation"][0] {
   ...,
   links[] {
@@ -36,19 +32,11 @@ export const EVENT_BY_SLUG_QUERY = groq`*[_type == "event" && slug.current == $s
   ...,
 }`;
 
-export const EVENTS_SLUGS_QUERY = groq`*[_type == "event" && defined(slug.current)]{
-  "slug": slug.current
-}`;
-
 export const FORM_BY_SLUG_QUERY = groq`*[_type == "form" && slug.current == $slug][0] {
   ...,
   content[] {
     ...,
   }
-}`;
-
-export const FORMS_SLUGS_QUERY = groq`*[_type == "form" && defined(slug.current)]{
-  "slug": slug.current
 }`;
 
 export const FORM_BY_ID_QUERY = groq`*[_type == "form" && _id == $id][0] {
