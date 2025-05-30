136b89e9a0404f9e6aac336a70228c3bdff61a03
James Blair <james@blair.nz>
2025-05-30 18:12:36 +1200

    Remove unused Button import from MainNav component to streamline code

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type { HEADER_QUERYResult } from "@/sanity/types";
