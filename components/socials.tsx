111e973092cce6d04bdaaf04e62860075a3b1acf
James Blair <james@blair.nz>
2025-05-31 00:35:06 +1200

    Improve mobile navigation layout and remove button around social menu item

--- components/socials.tsx
+++ components/socials.tsx
@@ -1,7 +1,6 @@
 import type { FOOTER_QUERYResult, HEADER_QUERYResult } from "@/sanity/types";
 import Link from "next/link";
 import { Image } from "./image";
-import { Button } from "./ui/button";
 
 export type SocialsProps = {
   socials:
@@ -13,11 +12,14 @@ export function Socials({ socials }: SocialsProps) {
   return (
     <div className="flex items-center gap-4">
       {socials.map((social) => (
-        <Button variant="ghost" size="icon" asChild key={social._key}>
-          <Link href={social.url} target="_blank" rel="noopener noreferrer">
-            <Image alt={social.title} image={social.icon} className="size-5" />
-          </Link>
-        </Button>
+        <Link
+          key={social._key}
+          href={social.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image alt={social.title} image={social.icon} className="size-5" />
+        </Link>
       ))}
     </div>
   );
