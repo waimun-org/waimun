7b6210ea5595f1de72241f62878fd43b26ef01d6
James Blair <james@blair.nz>
2025-06-08 02:04:12 +1200

    Update title template in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
   title: {
     default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
+    template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
 };
