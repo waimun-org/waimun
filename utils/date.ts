4e3537a2a2de8d6b93acebfd3b0680aaa06ef35c
James Blair <james@blair.nz>
2025-06-11 14:38:42 +1200

    Update date formatting in formatDate function to use single-digit day representation

--- utils/date.ts
+++ utils/date.ts
@@ -3,5 +3,5 @@ import { format } from "date-fns";
 export function formatDate(date: Date | string) {
   if (typeof date === "string") date = new Date(date);
   if (date.toString() === "Invalid Date") return "Invalid Date";
-  return format(date, "dd MMM yyyy");
+  return format(date, "d MMM yyyy");
 }
