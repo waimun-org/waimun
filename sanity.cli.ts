07442ba94ba6465ba5f8ad8c33c64a38ff6a4475
James Blair <james@blair.nz>
2026-03-18 12:43:28 +1300

    Upgrade packages and fix Sanity CLI env

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,15 @@
+import "dotenv/config";
+import { env } from "@/lib/env";
 import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: env.NEXT_PUBLIC_SANITY_DATASET,
+  },
+  typegen: {
+    path: "./**/*.{ts,tsx,js,jsx}",
+    schema: "./sanity/extract.json",
+    generates: "./sanity/types.ts",
   },
 });
