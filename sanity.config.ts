cbe5c7483ed2a04c5b65846354a83a67142e3791
James Blair <james@blair.nz>
2026-06-05 17:03:05 +0000

    fix: hardcode Sanity projectId/dataset for Studio deploy
    import.meta.env and process.env are not available during sanity deploy
    bundling. projectId and dataset are public values anyway.

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,21 +7,17 @@ import schemaTypes from "./sanity/schemaTypes/index";
 import { structure } from "./sanity/structure";
 import { colorInput } from "@sanity/color-input";
 
-const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID || process.env.PUBLIC_SANITY_PROJECT_ID;
-const dataset = import.meta.env.PUBLIC_SANITY_DATASET || process.env.PUBLIC_SANITY_DATASET;
-const apiVersion = import.meta.env.PUBLIC_SANITY_API_VERSION || process.env.PUBLIC_SANITY_API_VERSION;
-
 export default defineConfig({
   basePath: "/studio",
-  projectId: projectId!,
-  dataset: dataset!,
+  projectId: "qs4butxl",
+  dataset: "production",
   schema: {
     types: schemaTypes,
   },
   plugins: [
     structureTool({ structure }),
     visionTool({
-      defaultApiVersion: apiVersion!,
+      defaultApiVersion: "2025-05-11",
     }),
     colorInput(),
   ],
