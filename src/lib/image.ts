c321877e39a975c492c7120b95c0f86bbf760d95
James Blair <james@blair.nz>
2026-06-06 22:16:06 +1200

    chore: hardcode public site config (#7)

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,10 +1,9 @@
 import { createImageUrlBuilder } from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url";
-import { env } from "@/lib/env";
 
 const builder = createImageUrlBuilder({
-  projectId: env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: env.PUBLIC_SANITY_DATASET,
+  projectId: "qs4butxl",
+  dataset: "production",
 });
 
 export const urlFor = (source: SanityImageSource) => {
