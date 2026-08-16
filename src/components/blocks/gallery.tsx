e6fd13a0af23b7c8d6becde2c25c73ab236c3cc9
James Blair <james@blair.nz>
2026-08-16 21:17:59 +0000

    Update gallery column breakpoints

--- src/components/blocks/gallery.tsx
+++ src/components/blocks/gallery.tsx
@@ -24,7 +24,7 @@ export function Gallery({ block }: GalleryProps) {
       }}
     >
       <section className="container py-8 md:py-16">
-        <div className="columns-1 gap-4 sm:columns-2 md:columns-3 lg:columns-4">
+        <div className="columns-2 gap-4 sm:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6">
           {block.images.map((image) => (
             <Dialog.Trigger asChild key={image._key}>
               <button
@@ -38,7 +38,7 @@ export function Gallery({ block }: GalleryProps) {
                   alt={image.alt}
                   priority={image.priority}
                   className="h-auto w-full"
-                  sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+                  sizes="(min-width: 1536px) 17vw, (min-width: 1280px) 20vw, (min-width: 1024px) 25vw, (min-width: 640px) 33vw, 50vw"
                   widths={[320, 640, 960]}
                 />
               </button>
